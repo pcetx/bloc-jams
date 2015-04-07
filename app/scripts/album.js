e4a077fc82de80dbaa7f051a091cc7a15259ce54
Moulthun <moulthunl@gmail.com>
2015-04-07 04:33:33 +0000

    Added functionality to player controls

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -118,6 +118,49 @@
      $songList.append($newRow);
    }
  
+ }
+
+  var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left;
+ 
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ };
+
+
+ var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+
+    $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+
+        $seekBar.addClass('no-animate');
+
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    //cleanup
+    $(document).bind('mouseup.thumb', function(){
+
+        $seekBar.removeClass('no-animate');
+
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+ 
+  });
+ 
  };
 
  // This 'if' condition is used to prevent the jQuery modifications
@@ -128,6 +171,8 @@
    $(document).ready(function() {
 
     changeAlbumView(albumPicasso);
+
+    setupSeekBars();
 
     var albumList = [albumPicasso, albumMarconi];
 
