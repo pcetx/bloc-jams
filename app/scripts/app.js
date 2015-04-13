eb8899064db24f708a2d83cfca2fed5ec7175d86
Moulthun <moulthunl@gmail.com>
2015-04-13 05:52:11 +0000

    Converted landing page to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,38 @@
-require("./landing");
-require('./collection');
-require('./album');
-require('./profile');
+ //require('./landing');
+ //require('./album');
+ //require('./collection');
+ //require('./profile');
+ 
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the music up!";
+  $scope.mainText = "Bloc Jams";
+ 
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
 
+    $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+
+   $scope.mainTextClicked = function() {
+     shuffle($scope.albumURLs);
+   };
+
+ }]);
+
+
+ function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+shuffle([1,2,3]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -279,12 +279,44 @@ require.register("scripts/album", function(exports, require, module) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require('./profile');
+ //require('./landing');
+ //require('./album');
+ //require('./collection');
+ //require('./profile');
+ 
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the music up!";
+  $scope.mainText = "Bloc Jams";
+ 
+   $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
+
+    $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+
+   $scope.mainTextClicked = function() {
+     shuffle($scope.albumURLs);
+   };
+
+ }]);
 
 
+ function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+shuffle([1,2,3]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
